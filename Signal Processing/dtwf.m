d9d6af15827c787f21da7658da5313bbf7343187
ManashKalyonSahoo <110483416+ManashKalyonSahoo@users.noreply.github.com>
2023-07-12 11:48:24 -0400

    Added description

--- Signal Processing/dtwf.m
+++ Signal Processing/dtwf.m
@@ -1,5 +1,13 @@
 function dist = dtwf(x,y)
-% n = numel(x);
+% Description: This is not my function. This is a fix provided by
+% Srivardhan Gadila, so that the builtin DTW function is able to be passed
+% in as a handle to clustering functions such as @kmeans. Here is the link
+% to the MATLAB Answers where this fix was provided:
+% https://www.mathworks.com/matlabcentral/answers/742007-clustering-time-series-with-dtw
+
+% Author: Srivardhan Gadila
+
+
 m2 = size(y,1);
 dist = zeros(m2,1);
 for i=1:m2
